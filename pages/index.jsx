4437318c4c7a2f1b33cba81bc6f5d2028c000c07
marianorodriguez <mariano.rodriguez.232@gmail.com>
2019-06-30 15:22:10 -0300

    redux support with example store

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,48 @@
 
-import React from 'react';
-import Document from '../components/Document';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchGnomes } from '../store/gnomes/actions';
 
-export default () => (
-  <Document>
-      Hello World!
-  </Document>
-);
+class Index extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchGnomes());
+  }
+
+  render() {
+    const { loading, error, gnomes } = this.props;
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    if (error) {
+      return <div>{error}</div>;
+    }
+    return (
+      <div>
+        <ul>
+          {gnomes.map(gnome => <li key={gnome.id}>{gnome.name}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  gnomes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+Index.defaultProps = {
+  error: null,
+};
+
+const mapStateToProps = state => ({
+  loading: state.gnomes.loading,
+  error: state.gnomes.error,
+  gnomes: state.gnomes.items,
+});
+
+export default connect(mapStateToProps)(Index);
